e8d0fd038e452f9192a5030ef6a80d2e6233eb00
Mihail Sevagin <lolvillian@gmail.com>
2023-03-25 13:01:58 +0200

    Red&Green buttons
    Added buttons that colour bg accordingly

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -32,6 +32,30 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
+
+            Button red = new Button();
+            Button green = new Button();
+
+            red.Size = new Size(100, 50);
+            red.Location = new Point(20, 20);
+            red.Text = "Red";
+
+            green.Size = new Size(100, 50);
+            green.Location = new Point(20, 70);
+            green.Text = "Green";
+
+            red.Click += (e, s) =>
+            {
+                this.BackColor = Color.Red;
+            };
+
+            green.Click += (e, s) =>
+            {
+                this.BackColor = Color.Green;
+            };
+
+            this.Controls.Add(red);
+            this.Controls.Add(green);
         }
 
         #endregion
